c19a5f01181641acff3e5d74843e15daeb998bf6
root <root@newtaiga.asia-southeast1-b.c.buoyant-braid-229407.internal>
2019-02-09 13:40:45 +0000

    rev

--- taiga-postgres/install.sh
+++ taiga-postgres/install.sh
@@ -3,9 +3,9 @@ cd /home/taiga
 if [ ! -e setup.txt ]; then
     touch setup.txt
     cd /home/taiga/taiga-back
-    python manage.py migrate --noinput
-    python manage.py loaddata initial_user
-    python manage.py loaddata initial_project_templates
-    python manage.py compilemessages
-    python manage.py collectstatic --noinput
+    python3 manage.py migrate --noinput
+    python3 manage.py loaddata initial_user
+    python3 manage.py loaddata initial_project_templates
+#    python3 manage.py compilemessages
+#    python3 manage.py collectstatic --noinput
 fi
